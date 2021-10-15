edd3c9700e8ec8053efd57ed27c3ce294845afc8
Ryan Rivard <ryan.j.rivard@gmail.com>
2021-10-15 18:47:29 -0400

    Quick

--- Render.java
+++ Render.java
@@ -1,3 +1,34 @@
+import tester.*;                
+import javalib.worldimages.*;   
+import javalib.funworld.*;      
+import java.awt.Color;
+import java.util.Random; 
+
 /* Needed functions
   -drawShape
 */
+
+class WorldState extends World {
+  IList<InvaderColumn> invaders;
+  Spaceship spaceship;
+  IList<IBullet> bullets;
+
+
+  public WorldScene makeScene() {
+    // fold(convert invaders to list of images) + (spaceship image) + fold(convert bullets to images)
+    return invaders.map(new convertInvaderColumnsToImage())
+  }
+
+  //move the dots on the scene
+	public World onTick() {
+		ILoDot addDot = new ConsLoDot(new Dot(), this.dots);
+		return new Dots(addDot.move());
+	}
+
+	//move the dots on the scene
+	public World onTickTest() {
+		ILoDot addDot = new ConsLoDot(new Dot(new Random(600)), this.dots);
+		return new Dots(addDot.move());
+	}
+
+}
